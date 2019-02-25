36ad69ee172852a31c498146c98b7e1924cc97d9
Ludo <ludo_42800@hotmail.fr>
2019-02-25 14:19:00 +0100

    uiui

---
+++ app/Http/Controllers/ProduitController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProduitController extends Controller
+{
+    public function index()
+    {
+
+        $produits=\App\Produit::take(20)->get();
+
+
+        return view('produit')->withProduits($produits);
+    }
+}
